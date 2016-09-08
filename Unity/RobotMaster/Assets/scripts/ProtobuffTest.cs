97db16c340bf1200615fe445b6852f6dfd6d49fb
Jorri Fransen <jorri.fransen@student.fontys.nl>
2016-09-09 00:51:39 +0200

    Google protocol buffer example working in Unity with protobuf-net.

---
+++ Unity/RobotMaster/Assets/scripts/ProtobuffTest.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using ProtoBuf;
+using protospike;
+using System.IO;
+
+public class ProtobuffTest : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        Person loadedPerson;
+        using (var file = File.OpenRead("john.dat"))
+        {
+            loadedPerson = Serializer.Deserialize<Person>(file);
+        }
+
+        Debug.Log(loadedPerson);
+
+        Debug.Log(loadedPerson.name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
