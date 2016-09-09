9ad090ddfe2a3efe24fa18fcc42e25d5503320d8
justus4all <justus.vandijk@student.fontys.nl>
2016-09-09 15:08:01 +0200

    add Unity UI

---
+++ Unity/RobotMaster/Assets/scripts/Robot/RobotSelect.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RobotSelect : MonoBehaviour {
+
+    public bool isSelected;
+    public bool particleActive;
+
+    public GameObject particle;
+    private GameObject particleObject;
+
+    void Start ()
+    {
+        isSelected = false;
+        particleActive = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    if (isSelected && !particleActive)
+	    {
+	        Debug.Log("kaasstengel");
+	        
+	        {
+	            particleObject = (GameObject)Instantiate(particle, this.transform.position, this.transform.rotation);
+	            particleActive = true;
+	        }
+	    }
+	    if (!isSelected)
+	    {
+	        particleActive = false;
+            Destroy(particleObject);
+	    }
+	}
+}
