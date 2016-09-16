5f7df2e75ec5d82a2b365d2260c9293232386f51
Jorri Fransen <jorri.fransen@student.fontys.nl>
2016-09-16 11:50:55 +0200

    Unity Test Tools added to unity assets. Also included an example unit test.

---
+++ Unity/RobotMaster/Assets/scripts/Testscripts/Editor/UnitTestExample.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using NUnit.Framework;
+
+[TestFixture]
+public class UnitTestExample {
+    
+    [Test]
+    public void ExampleTestPasses()
+    {
+        Assert.That(true);
+    }
+
+    [Test]
+    public void ExampleTestFails()
+    {
+        Assert.That(false);
+    }
+}
