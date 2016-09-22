fb2d311c351bd14b7cbc5beaa78fbda23eb81629
justus4all <justus.vandijk@student.fontys.nl>
2016-09-22 14:06:41 +0200

    deleted some stuff, added some other stuff

--- Unity/RobotMaster/Assets/scripts/Communication/IMessageReceiver.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public interface IMessageReceiver
-{
-
-    void IncommingMessage(Message newMessage);
-
-}
--- Unity/RobotMaster/Assets/scripts/Communication/IMessageSender.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public interface IMessageSender
-{
-    bool SendCommand(Message msg);
-}
