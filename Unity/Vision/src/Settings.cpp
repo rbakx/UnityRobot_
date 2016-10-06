6cecabf098c61cc996736ab37de020575746f3a0
Rutger Rauws <r.rauws@student.fontys.nl>
2016-10-06 14:55:30 +0200

    Wrote basic settings file (de)serialization

---
+++ Unity/Vision/src/Settings.cpp
@@ -0,0 +1,71 @@
+#include "Settings.h"
+
+using namespace std;
+using namespace cv;
+
+////These write and read functions must be defined for the serialization in FileStorage to work
+//static void write(FileStorage& fs, const std::string&, const Settings& x)
+//{
+//    x.write(fs);
+//}
+//
+//static void read(const FileNode& node, Settings& x, const Settings& default_value = Settings()){
+//    if(node.empty())
+//        x = default_value;
+//    else
+//        x.read(node);
+//}
+
+void Settings::write(const string fileName) const
+{
+    FileStorage fs(fileName, FileStorage::WRITE);
+
+    fs  << "Settings"
+        << "{"
+            << "Device"
+            << "{"
+                << "number" << dp.number
+                << "pid" << dp.pid
+                << "vid" << dp.vid
+            << "}"
+            << "Recording"
+            << "{"
+               << "width" << rp.width
+               << "height" << rp.height
+               << "fps" << rp.fps
+            << "}"
+        << "}";
+
+    fs.release();
+}
+
+Settings Settings::read(const string fileName)
+{
+    Settings settingsObj;
+
+    FileStorage fs(fileName, FileStorage::READ);
+
+    FileNode settingsNode = fs["Settings"];
+    FileNode deviceNode = settingsNode["Device"];
+    FileNode recordingNode = settingsNode["Recording"];
+
+    if(deviceNode.isNone() || recordingNode.isNone()) //We couldn't find the properties in the file
+        throw runtime_error("Settings are incomplete!");
+
+    settingsObj.dp = DeviceProperties(deviceNode["number"], deviceNode["pid"], deviceNode["vid"]);
+    settingsObj.rp = RecordingProperties(recordingNode["width"], recordingNode["height"], recordingNode["fps"]);
+
+    fs.release();
+
+    return settingsObj;
+}
+
+const DeviceProperties& Settings::getDeviceProperties()
+{
+    return dp;
+}
+
+const RecordingProperties& Settings::getRecordingProperties()
+{
+    return rp;
+}
---
+++ Unity/Vision/src/Settings.h
@@ -0,0 +1,43 @@
+#ifndef ASSIGNMENT_SETTINGS_H
+#define ASSIGNMENT_SETTINGS_H
+
+#include <opencv2/core/core.hpp>
+
+struct DeviceProperties {
+    int number;
+    int vid;
+    int pid;
+
+    DeviceProperties() : number(-1), vid(-1), pid(-1) {}
+    DeviceProperties(int number, int vid, int pid) : number(number), vid(vid), pid(pid) {}
+};
+
+struct RecordingProperties {
+    int width;
+    int height;
+    int fps;
+
+    RecordingProperties() : width(-1), height(-1), fps(-1) {}
+    RecordingProperties(int width, int height, int fps) : width(width), height(height), fps(fps) {}
+};
+
+class Settings {
+private:
+    Settings() {};
+    DeviceProperties dp;
+    RecordingProperties rp;
+
+public:
+    Settings(DeviceProperties dp, RecordingProperties rp) : dp(dp), rp(rp) {};
+    void write(const std::string fileName) const;
+    static Settings read(const std::string fileName);
+
+    /*
+     * Getters
+     */
+    const DeviceProperties& getDeviceProperties();
+    const RecordingProperties& getRecordingProperties();
+};
+
+
+#endif //ASSIGNMENT_SETTINGS_H
