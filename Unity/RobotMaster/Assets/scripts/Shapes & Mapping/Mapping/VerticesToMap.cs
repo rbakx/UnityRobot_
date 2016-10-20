54e3d7108e730f75f30fc13b1d2593bf045070b5
justus4all <justus.vandijk@student.fontys.nl>
2016-10-20 13:41:43 +0200

    assets n stuff

---
+++ Unity/RobotMaster/Assets/scripts/Shapes & Mapping/Mapping/VerticesToMap.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VerticesToMap : MonoBehaviour
+{
+    void Update()
+    {
+    }
+
+    public Vector2[] V2E;
+    public Mesh mesh;
+
+    public void VerticesToObjects(Vector2[] verticesToExtrude) { 
+    mesh = GetComponent<MeshFilter>().mesh;
+        Vector3[] vertices = mesh.vertices;
+        int i = 0;
+        while (i < vertices.Length)
+        {
+            for(int j = 0; j < verticesToExtrude.Length; j++)
+            {
+                if(vertices[i].x == verticesToExtrude[j].x && vertices[i].z == verticesToExtrude[j].y && verticesToExtrude[j] != new Vector2(-1,-1))
+                {
+                    vertices[i].y += 100;
+                    verticesToExtrude[j] = new Vector2(-1, -1);
+                }
+                j++;
+            }
+            i++;
+        }
+        mesh.vertices = vertices;
+        mesh.RecalculateBounds();
+    }
+}
+
