d4a8ff6fe3db05ff0f665b64d778712bf47cd75d
justus4all <justus.vandijk@student.fontys.nl>
2016-11-10 10:15:18 +0100

    add new ui elements, demo shapes from 2d data

--- Unity/RobotMaster/Assets/scripts/Shapes & Mapping/Mapping/DebugPointsToModelBuilder.cs
+++ Unity/RobotMaster/Assets/scripts/Shapes & Mapping/Mapping/DebugPointsToModelBuilder.cs
@@ -22,9 +22,11 @@ public class DebugPointsToModelBuilder : MonoBehaviour
             {
                 PointsToModelBuilder builder = GetComponent<PointsToModelBuilder>();
 
-                List<Vector3> SurfaceV3 = new List<Vector3>(surface.Count);
+                List<Vector2> _sf = surface;
 
-                foreach (Vector2 val in surface)
+                List<Vector3> SurfaceV3 = new List<Vector3>(_sf.Count);
+
+                foreach (Vector2 val in _sf)
                 {
                     SurfaceV3.Add(new Vector3(val.x, 0.0F, val.y));
                 }
@@ -35,6 +37,40 @@ public class DebugPointsToModelBuilder : MonoBehaviour
 
             yield return new WaitForSeconds(0.1F);
         }
+    }
+
+
+    public void SetShape1()
+    {
+        surface = new List<Vector2>();
+        surface.Add(new Vector2(0, 3));
+        surface.Add(new Vector2(3, 5));
+        surface.Add(new Vector2(6, 3));
+        surface.Add(new Vector2(4.5f, 0));
+        surface.Add(new Vector2(3,-1));
+        surface.Add(new Vector2(1.5f, 0));
+
+        Debug.Log("Number of vertices:" + surface.Count);
+        UpdateModel = true;
+    }
+
+    public void SetShape2()
+    {
+        surface = new List<Vector2>();
+        surface.Add(new Vector2(0, 3));
+        surface.Add(new Vector2(2f, 3));
+        surface.Add(new Vector2(3, 5));
+        surface.Add(new Vector2(4f, 3));
+        surface.Add(new Vector2(6, 3));
+        surface.Add(new Vector2(4, 1.5f));
+        surface.Add(new Vector2(4.5f, 0));
+        surface.Add(new Vector2(3, 1));
+        surface.Add(new Vector2(1.5f, 0));
+        surface.Add(new Vector2(2, 2));
+
+        Debug.Log("Number of vertices:" + surface.Count);
+
+        UpdateModel = true;
     }
 }
 
