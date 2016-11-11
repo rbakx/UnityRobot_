636ec318dd043db55e41d86a1e60b55f4b650e5a
Rutger Rauws <r.rauws@student.fontys.nl>
2016-11-11 14:49:23 +0100

    Updated .gitignore, removed contents of build folder

--- Com-lib/UnityRobot_CommLib/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /shared/OneDrive/Documents/Studie/Fontys/Q12/PROJ/UnityRobot/Com-lib/UnityRobot_CommLib_better_maybe??
-# Build directory: /shared/OneDrive/Documents/Studie/Fontys/Q12/PROJ/UnityRobot/Com-lib/UnityRobot_CommLib_better_maybe??/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(spdlog)
-subdirs(src/Networking)
--- Com-lib/UnityRobot_CommLib/build/src/Networking/CMakeFiles/Networking.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "libNetworking.pdb"
-  "libNetworking.a"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/Networking.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
