8dd1d0141875221e321c47f7448ec4947a8d6e02
Jorri Fransen <jorri.fransen@student.fontys.nl>
2016-11-17 13:13:32 +0100

    [Robot] broker <-> robot communication working sometimes.

---
+++ robot/ev3-broker/ev3-broker/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using EV3WifiLib;
+
+namespace ev3_broker
+{
+    class Program
+    {
+        static void fatalError(string msg)
+        {
+            Console.WriteLine(msg);
+            Console.ReadLine();
+            Environment.Exit(41);
+        }
+
+        static void Main(string[] args)
+        {
+            EV3Wifi ev3 = new EV3Wifi();
+            string conStatus = ev3.Connect();
+
+            if (conStatus != "ok")
+            {
+                fatalError("Failed to connect to ev3");
+            }
+            else
+            {
+                Console.WriteLine("Ev3 connected");
+            }
+
+            Console.Out.Flush();
+
+
+            //while (true)
+            //{
+            //    ev3.SendMessage("get_button", "STATUS");
+            //    string res = ev3.ReceiveMessage("EV3Wifi", "BUTTON");
+            //    Console.WriteLine("Button: ", res);
+            //    Console.Out.Flush();
+
+            //    ConsoleKeyInfo cki = Console.ReadKey();
+            //    if (cki.Key == ConsoleKey.Escape)
+            //    {
+            //        break;
+            //    }
+            //}
+
+            ev3.Disconnect();
+
+            Console.ReadLine();
+        }
+    }
+}
