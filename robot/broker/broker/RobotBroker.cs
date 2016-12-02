222aba26b31a429f1d9ae93a2dddaa15b5960ba2
Jorri Fransen <jorri.fransen@student.fontys.nl>
2016-12-02 11:08:00 +0100

    [Broker] RobotBroker class.

---
+++ robot/broker/broker/RobotBroker.cs
@@ -0,0 +1,110 @@
+﻿using Networking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace broker
+{
+    public class RobotBroker : IDisposable
+    {
+        private bool _connected;
+
+        private GeneralTypeRobot _robot;
+
+        private IPresentationProtocol _protocol;
+        private IDataLink _dataLink;
+        private Communicator _communicator;
+
+        private bool _disposed;
+
+        public Communicator Communicator { get { return _communicator; } }
+
+        public RobotBroker()
+        {
+            _connected = false;
+
+            _robot = null;
+
+            _protocol = null;
+            _dataLink = null;
+            _communicator = null;
+
+            _disposed = false;
+        }
+
+        public bool Connect(string unityIp, ushort unityPort, int timeout = -1)
+        {
+            //Creates a connection to the robot using the specified hostname and port
+            TcpClient tcpClient = new TcpClient();
+            IAsyncResult ar = tcpClient.BeginConnect(unityIp, unityPort, null, null);
+            WaitHandle wh = ar.AsyncWaitHandle;
+            try
+            {
+                if (!wh.WaitOne(timeout, false))
+                {
+                    tcpClient.Close();
+                    wh.Close();
+                    return false;
+                }
+
+                tcpClient.EndConnect(ar);
+
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+            finally
+            {
+                wh.Close();
+            }
+
+            _dataLink = new TCPDataLink(tcpClient);
+            _protocol = new ProtoBufPresentation();
+            _dataLink.SetReceiver(_protocol);
+
+            Communicator communicator = new Communicator(_dataLink, _protocol);
+
+            _connected = true;
+            return true;
+        }
+
+        public bool ConnectRobot(GeneralTypeRobot robot)
+        {
+            if (robot == null)
+            {
+                throw new ArgumentNullException("robot");
+            }
+
+            if (!_connected)
+            {
+                throw new InvalidOperationException("Can't connect robot without being connected");
+            }
+
+            _robot = robot;
+            MessageActionMapper map = new MessageActionMapper(_robot);
+            _protocol.SetReceiver(map);
+
+            return true;
+        }
+
+        public void Dispose()
+        {
+            if (!_disposed)
+            {
+                if (_connected && _dataLink != null)
+                {
+                    _dataLink.Dispose();
+                }
+
+                _disposed = true;
+                _connected = false;
+            }
+        }
+    }
+}
--- robot/ev3-broker/ev3-broker/Program.cs
+++ robot/ev3-broker/ev3-broker/Program.cs
@@ -1,4 +1,5 @@
 ﻿using broker;
+using Communication;
 using Networking;
 using System;
 using System.Net.Sockets;
@@ -9,70 +10,34 @@ namespace ev3_broker
     {
         static void Main(string[] args)
         {
-            //using (EV3Robot ev3 = new EV3Robot(null, "ev3 robot"))
-            //{
-            //while (true)
-            //{
-            //    ev3.Indicate();
-            //    Console.ReadLine();
-            //}
-
-
-            //ev3.VelocitySet(MessageBuilder.CreateVector(20, 0, 0), MessageBuilder.CreateVector(0, 0, 0));
-            //Console.ReadLine();
-            ////ev3.VelocitySet(MessageBuilder.CreateVector(0, 0, 0), MessageBuilder.CreateVector(0, 0, -20));
-            //Console.ReadLine();
-            //ev3.VelocitySet(MessageBuilder.CreateVector(0, 0, 0), MessageBuilder.CreateVector(0, 0, 0));
-
-            //}
-
-            //Gets the hostname and port from the command line parameters to connect to the robot in question (in this case the Nao)
+            //Gets the hostname and port from the command line parameters to connect to the robot in question(in this case the Nao)
             string hostname = args.Length > 0 ? args[0] : "192.168.0.102";
-            int port = args.Length > 1 ? Int32.Parse(args[1]) : 1234;
-
-            //Creates a connection to the robot using the specified hostname and port
-            TcpClient tcpClient;
-            try
-            {
-                tcpClient = new TcpClient(hostname, port);
-            }
-            catch (SocketException ex)
-            {
-                Console.WriteLine(ex.Message);
-                Console.ReadLine();
-                return;
-            }
+            ushort port = (ushort)(args.Length > 1 ? Int32.Parse(args[1]) : 1234);
 
             Ev3Connection ev3Con = new Ev3Connection("EV3Wifi");
-            if (!ev3Con.Connect(5000))
+            if (false || !ev3Con.Connect(5000))
             {
-                throw new Exception("EV3 not connected");
+                Console.WriteLine("Failed to connect with EV3");
             }
-            Console.WriteLine("Ev3 connected");
-
-            IDataLink dl = new TCPDataLink(tcpClient);
-            IPresentationProtocol pp = new ProtoBufPresentation();
-            dl.SetReceiver(pp);
-
-            Communicator communicator = new Communicator(dl, pp);
-
-            EV3Robot ev3 = new EV3Robot(communicator, "My little robot", ev3Con);
-            MessageActionMapper map = new MessageActionMapper(ev3);
-            pp.SetReceiver(map);
-
-            //Waiting for user input before closing
-            Console.WriteLine("Press enter to close the connection.");
-            while (true)
+            else
             {
-                if (Console.ReadKey().Key == ConsoleKey.Enter)
-                    break;
+                Console.WriteLine("Ev3 connected");
+
+                using (RobotBroker broker = new RobotBroker())
+                {
+                    if (broker.Connect(hostname, port, 5000))
+                    {
+                        using (EV3Robot ev3 = new EV3Robot(broker.Communicator, "My little robot", ev3Con))
+                        {
+                            broker.ConnectRobot(ev3);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Failed to connect with unity");
+                    }
+                }
             }
-
-            //Clean up
-            ev3.Dispose();
-            dl.Dispose();
-            tcpClient.Close();
-
             Console.ReadLine();
         }
     }
