5e2c1ea7acf06cdd9eba9b8a98f04c4f6ffd2ab6
Jorri Fransen <jorri.fransen@student.fontys.nl>
2016-12-09 11:35:00 +0100

    Merged standalone and unity networking code.

---
+++ networking/networking/networking/LocalDataLink.cs
@@ -0,0 +1,25 @@
+﻿using Networking;
+
+public class LocalDataLink : IDataLink
+{
+
+    public bool Connected()
+    {
+        return true;
+    }
+
+    public void Dispose()
+    {
+
+    }
+
+    public bool SendData(byte[] data)
+    {
+        return true;
+    }
+
+    public void SetReceiver(IDataStreamReceiver receiver)
+    {
+        
+    }
+}
